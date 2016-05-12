e545d55182c04924933efa37be176c79e84a3e2f
Esben Sandbæk Steffensen <esbensand@hotmail.com>
2016-05-12 14:37:38 +0200

    Base Tower Func

---
+++ BaseBuilder/Source/BaseBuilder/StandardTower.cpp
@@ -0,0 +1,86 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BaseBuilder.h"
+#include "StandardTower.h"
+#include "DrawDebugHelpers.h"
+
+// Sets default values
+AStandardTower::AStandardTower()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	/*CubeMesh = CreateAbstractDefaultSubobject<UStaticMeshComponent>(TEXT("Box"));
+
+	static ConstructorHelpers::FObjectFinder <UStaticMesh>StaticMesh(TEXT("StaticMesh'/Game/Geometry/Meshes/1M_Cube.1M_Cube'"));
+
+	CubeMesh->SetStaticMesh(StaticMesh.Object);
+
+	CubeMesh->bHiddenInGame = false;
+
+	RootComponent = CubeMesh;*/
+}
+
+// Called when the game starts or when spawned
+void AStandardTower::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void AStandardTower::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (AttackTimer <= 0)
+	{
+		Attack();
+	}
+	else
+	{
+		AttackTimer -= DeltaTime;
+	}
+}
+
+float AStandardTower::TakeDamage(float Damage, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, class AActor* DamageCauser)
+{
+	if (DamageCauser->GetClass() != GetClass())
+	{
+		const float ActualDamage = Super::TakeDamage(Damage, DamageEvent, EventInstigator, DamageCauser);
+		if (ActualDamage > 0.f)
+		{
+			Health -= ActualDamage;
+		}
+
+		return ActualDamage;
+	}
+	return 0;
+}
+
+void AStandardTower::Attack()
+{
+	FVector StartPoint = FVector(GetActorLocation().X, GetActorLocation().Y, GetActorLocation().Z + 90);
+
+	FRotator Rotation = FRotator(GetActorRotation().Pitch, GetActorRotation().Yaw + 90, GetActorRotation().Roll);
+
+	FVector EndPoint = StartPoint + (Rotation.Vector() * 200);
+
+	TArray<FHitResult> HitResult = TArray<FHitResult>();
+
+	DrawDebugLine(GetWorld(), StartPoint, EndPoint, FColor(255, 0, 0), true, -1.0f, 0, 10.0f);
+
+	bool Hit = GetWorld()->LineTraceMultiByObjectType(HitResult, StartPoint, EndPoint, FCollisionObjectQueryParams(ECollisionChannel::ECC_Pawn), FCollisionQueryParams("ActionTrace", false, GetOwner()));
+
+	if (Hit)
+	{
+		for (int i = 0; i < HitResult.Num(); i++)
+		{
+			TSubclassOf<UDamageType> const ValidDamageTypeClass = TSubclassOf<UDamageType>(UDamageType::StaticClass());
+			FDamageEvent DamageEvent(ValidDamageTypeClass);
+
+			HitResult[i].Actor->TakeDamage(Damage, DamageEvent, NULL, this);
+
+			AttackTimer = AttackSpeed;
+
+		}
+	}
+}
---
+++ BaseBuilder/Source/BaseBuilder/StandardTower.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "BaseBuilding.h"
+#include "StandardTower.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BASEBUILDER_API AStandardTower : public ABaseBuilding
+{
+	GENERATED_BODY()
+	
+public:
+	// Sets default values for this actor's properties
+	AStandardTower();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Called every frame
+	virtual void Tick(float DeltaSeconds) override;
+	
+	virtual float TakeDamage(float Damage, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, class AActor* DamageCauser) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float Health;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float AttackSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float Damage;
+private:
+
+	float AttackTimer;
+
+	virtual void Attack();
+};
